1e1efbca125fa3c7a3b7cac2c3374727b6a9898f
Stefan Kopco <stefan.kopco@vent.io>
2024-10-01 20:55:17 +0200

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,29 @@
 import { defineConfig, envField } from "astro/config"
 import storyblok from '@storyblok/astro'
-// import { loadEnv } from 'vite'
 import tailwind from '@astrojs/tailwind'
 import node from '@astrojs/node';
 import mkcert from 'vite-plugin-mkcert'
-
-// const env = loadEnv('', process.cwd(), 'STORYBLOK')
-
 
 
 export default defineConfig({
   env: {
     schema: {
-      // CLIENT_API_URL: envField.string({ context: "client", access: "public" }),
-      // SERVER_API_URL: envField.string({ context: "server", access: "public" }),
       STORYBLOK_API_TOKEN: envField.string({ context: "server", access: "secret" }),
     }
   },
+
   integrations: [
     storyblok({
-      //accessToken: env.STORYBLOK_TOKEN,
       accessToken: process.env.STORYBLOK_API_TOKEN,
+      contentLayer: true,
+      // enableFallbackComponent: true,
       apiOptions: {
         region: 'eu',
       },
-      livePreview: true,
-      bridge: {
-        customParent: 'https://app.storyblok.com',
-      },
+      // livePreview: true,
+      // bridge: {
+      //   customParent: 'https://app.storyblok.com',
+      // },
       components: {
         page: 'storyblok/Page',
         feature: 'storyblok/Feature',
